8ec8fd7a7d4952001045ed7be236d013ddca82d4
Tuval Lifshitz <tuval.lif@gmail.com>
2017-08-20 14:42:16 +0300

    Adding class User outside userdb

---
+++ GamingServer/src/User.cpp
@@ -0,0 +1,25 @@
+/*
+ * User.cpp
+ *
+ *  Created on: Aug 20, 2017
+ *      Author: user
+ */
+
+#include <src/User.h>
+
+using namespace std;
+
+namespace npl {
+
+
+	User::User(string username, TCPSocket * socket){
+		this->username = username;
+		this->socket = socket;
+		this->state = STATE_DEFAULT;
+	}
+
+	User::~User() {
+	// TODO Auto-generated destructor stub
+	}
+
+}
---
+++ GamingServer/src/User.h
@@ -0,0 +1,37 @@
+/*
+ * User.h
+ *
+ *  Created on: Aug 20, 2017
+ *      Author: user
+ */
+
+#ifndef USER_H_
+#define USER_H_
+
+#include <map>
+#include <list>
+#include <string>
+#include "TCPSocket.h"
+
+using namespace std;
+
+namespace npl {
+
+class User {
+public:
+
+	static const int STATE_DEFAULT = 0;
+	static const int STATE_SEEKING = 1;
+	static const int STATE_PLAYING = 2;
+	static const int STATE_BUSY = 3;
+
+	User(	string username, TCPSocket * socket);
+
+
+
+	string username;
+	TCPSocket * socket;
+
+	int state;
+};
+#endif /* USER_H_ */
